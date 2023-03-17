d440b30cca34e72d6c46137aaec594056b302bdd
Enock-Yegon <ykibet70@gmail.com>
2023-03-17 18:22:22 +0300

    Completed Assignment

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,4 +1,6 @@
 class AuthorsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
   
   def show
     author = Author.find(params[:id])
@@ -7,15 +9,26 @@ class AuthorsController < ApplicationController
   end
 
   def create
-    author = Author.create(author_params)
-
+    # create! exceptions will be handled by the rescue_from ActiveRecord::RecordInvalid code
+    author = Author.create!(author_params)
     render json: author, status: :created
   end
 
   private
   
-  def author_params
-    params.permit(:email, :name)
+  def find_author
+    Author.find(params[:id])
   end
-  
+
+  def author_params
+    params.permit(:name, :email)
+  end
+
+  def render_not_found_response
+    render json: { error: "Bird not found" }, status: :not_found
+  end
+
+  def render_unprocessable_entity_response(invalid)
+    render json: { errors: invalid.record.errors }, status: :unprocessable_entity
+  end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
 
   def show
     post = Post.find(params[:id])
@@ -7,17 +9,27 @@ class PostsController < ApplicationController
   end
 
   def update
-    post = Post.find(params[:id])
-
-    post.update(post_params)
-
+    post = find_post
+    # update! exceptions will be handled by the rescue_from ActiveRecord::RecordInvalid code
+    post.update!(post_params)
     render json: post
   end
 
   private
 
+  def find_post
+    Post.find(params[:id])
+  end
+
   def post_params
     params.permit(:category, :content, :title)
   end
 
+  def render_not_found_response
+    render json: { error: "Bird not found" }, status: :not_found
+  end
+
+  def render_unprocessable_entity_response(invalid)
+    render json: { errors: invalid.record.errors }, status: :unprocessable_entity
+  end
 end
